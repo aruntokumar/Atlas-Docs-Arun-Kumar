48f61affc7a478ceee100b2f5774f1915f64715f
aruntokumar <82315613+aruntokumar@users.noreply.github.com>
2025-06-07 09:26:03 +0530

    Add files via upload

---
+++ Task019.java
@@ -0,0 +1,7 @@
+public class task019 {
+    public static void main(String[] args) {
+        task019 myObj = new task019();
+        myObj.setName("Arun");
+        System.out.println(myObj.getName());
+    }
+}
